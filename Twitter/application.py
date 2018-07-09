cc805ad48d1ab8c0d0520fdb41841bb716f7eb61
savio singh <savi895@gmail.com>
2018-07-09 20:20:30 +0000

    Second commit where we are adding list items dynamicaly

--- Twitter/application.py
+++ Twitter/application.py
@@ -1,3 +1,12 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+@app.route('/login')
+def login():
+    return render_template('layout.html')
+
 
 def predict(sentence):
     return "positive"
