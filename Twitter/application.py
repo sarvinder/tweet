0092e343f449330b0d60b55ff415cbaa7fcc6bb3
sarvinder <savi895@gmail.com>
2018-07-12 09:25:06 -0800

    adding the server request from json

--- Twitter/application.py
+++ Twitter/application.py
@@ -1,12 +1,20 @@
-from flask import Flask,render_template
+from flask import Flask, jsonify, render_template, request
 
 app=Flask(__name__)
 
 @app.route('/')
 @app.route('/login')
 def login():
+    print('rendering......')
     return render_template('layout.html')
 
 
-def predict(sentence):
-    return "positive"
+@app.route('/predict',methods=['POST', 'GET'])
+def predict():
+    if request.method=='POST':
+        print('inside the post method')
+        a = request.request.args.get('x')
+        print('got the data')
+    else:
+        print('the method was not called with pos')
+    return jsonify(result="positive")
